698d13dc6e28409b3987a0181e39f51377b1f461
ductnf8 <ductnf8@fullstack.edu.vn>
2025-06-14 12:19:34 +0700

    nnn

---
+++ Estate-Website/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from "./components/Header.jsx";
+import About from "./components/About.jsx";
+
+
+const App = () => {
+    return (
+        <div className='w-full overflow-hidden'>
+            <Header/>
+            <About/>
+        </div>
+    )
+}
+
+export default App
+
+
